7e5e94ae9b7041f394b1fcb96e663fac62d5687f
Srija S <srijakumar7@gmail.com>
2018-08-05 23:11:20 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
 
   attr_accessor :name, :breed, :id
 
-  def initialize(name, breed, id = nil)
+  def initialize(name:, breed:, id: nil)
     @name = name
     @breed = breed
     @id = id
@@ -16,7 +16,7 @@ class Dog
   def self.create
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS dogs
-    id PRIMARY 
+    id PRIMARY
     SQL
   end
 
